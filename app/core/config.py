7c9586ff6a4a586188d7952475e59b5c3276ab3f
guitabalipa <guilherme.tabalipa@snowmanlabs.com>
2020-08-28 14:44:48 -0300

    setup docker

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ import secrets
 from typing import Any, Dict, List, Optional, Union
 from dotenv import load_dotenv
 
-from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
+from pydantic import AnyHttpUrl, BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
@@ -10,6 +10,10 @@ class Settings(BaseSettings):
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+
+    SERVER_NAME: str
+    SERVER_HOST: AnyHttpUrl
+    PROJECT_NAME: str
 
     AWS_PRE_SIGNED_URL_EXPIRE_MINUTES: int = 60 * 24
     AWS_ACCESS_KEY_ID: str
@@ -34,6 +38,16 @@ class Settings(BaseSettings):
             host=values.get("POSTGRES_SERVER"),
             path=f"/{values.get('POSTGRES_DB') or ''}",
         )
+
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
 
 
 load_dotenv()
